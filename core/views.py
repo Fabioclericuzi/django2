83da2d2e6f403d0bf81237a38f799e5037b6a7fc
unknown <fabioclericuzilima@hotmail.com>
2024-02-29 20:54:01 -0300

    Criação de formulário para preenchimento pelo usuário e formatação de arquivos para tal função

--- core/views.py
+++ core/views.py
@@ -1,12 +1,36 @@
+from django.contrib import messages
 from django.shortcuts import render
-
+from .forms import ContatoForm
 
 def index(request):
     return render(request, 'index.html')
 
 
 def contato(request):
-    return render(request, 'contato.html')
+    form = ContatoForm(request.POST or None)
+
+    if str(request.method) == 'POST':
+        print(f'POST: {request.POST}')
+        if form.is_valid():
+            nome = form.cleaned_data['nome']
+            email = form.cleaned_data['email']
+            assunto = form.cleaned_data['assunto']
+            mensagem = form.cleaned_data['mensagem']
+
+            print('Mensagem enviada')
+            print(f'Nome: {nome}')
+            print(f'E-mail: {email}')
+            print(f'Assunto {assunto}')
+            print(f'Mensagem: {mensagem}')
+
+            messages.success(request, 'E-mail enviado com sucesso')
+
+        else:
+            messages.error(request, 'Erro ao enviar o E-mail')
+    context = {
+        'form': form
+    }
+    return render(request, 'contato.html', context)
 
 
 def produto(request):
